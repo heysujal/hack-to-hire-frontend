4e2531c058700f06369ba3a4fe56a72db68bf5d9
Sujal Gupta <sujalgupta6100@gmail.com>
2024-07-27 21:44:48 +0530

    init
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Globe from "./components/Globe/magicui/globe";
+
+function App() {
+  return (
+    <div>
+      <Globe className='globe-bg'/>
+
+      <h1 className="text-3xl font-bold welcome-msg">
+        Welcome to IndiGo Flight Tracker
+      </h1>
+    </div>
+  );
+}
+
+export default App;
